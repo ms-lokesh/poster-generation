aa977c6b339ffb172d072e0e6a552e50926fe6ed
Lokesh Kumar M S <lokeshkumar19.4.2007@gmail.com>
2026-08-20 10:29:11 +0530

    update the content

--- backend/image_processor.py
+++ backend/image_processor.py
@@ -4,14 +4,17 @@ from PIL import Image, ImageDraw, ImageOps
 class ImageProcessor:
     def __init__(self, template_path):
         self.template_path = template_path
-        # New template: 1122x1713, left oriented
-        self.target_size = (450, 450)
-        self.paste_coords = (55, 600)
+        # New template: 499x755, upscaled by 2x -> 998x1510
+        self.target_size = (400, 400)
+        self.paste_coords = (100, 460)
 
     def process_and_merge(self, user_img_path, output_path, name=None, institution=None):
         try:
             user_img = Image.open(user_img_path).convert("RGBA")
             template_img = Image.open(self.template_path).convert("RGBA")
+            
+            # Upscale template for HD quality
+            template_img = template_img.resize((template_img.width * 2, template_img.height * 2), Image.Resampling.LANCZOS)
             
             # Center crop the user image to the target size
             user_img = ImageOps.fit(user_img, self.target_size, method=Image.Resampling.LANCZOS, centering=(0.5, 0.5))
@@ -23,13 +26,10 @@ class ImageProcessor:
             
             user_img.putalpha(mask)
             
-            # We paste the template ON TOP of the user image if the template has a transparent hole.
-            # But wait, it's easier to create a background canvas, paste the user image, then paste the template.
-            # Let's assume the template is a full poster with a transparent circle.
             # Create a background canvas
             final_img = Image.new("RGBA", template_img.size, (255, 255, 255, 255))
             
-            # Paste the template first (since it doesn't have a transparent hole)
+            # Paste the template first
             final_img.paste(template_img, (0, 0))
             
             # Paste the user image ON TOP of the template using the circular mask
@@ -40,7 +40,7 @@ class ImageProcessor:
                 draw = ImageDraw.Draw(final_img)
                 
                 max_text_width = 500
-                font_size = 90
+                font_size = 80
                 font = None
                 
                 def get_text_width(f, text):
@@ -49,7 +49,7 @@ class ImageProcessor:
                     except AttributeError:
                         return draw.textsize(text, font=f)[0]
                 
-                while font_size >= 20:
+                while font_size >= 24:
                     try:
                         font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial Bold.ttf", font_size)
                     except:
@@ -65,21 +65,21 @@ class ImageProcessor:
                 
                 # Calculate text position (center below the photo)
                 photo_center_x = self.paste_coords[0] + (self.target_size[0] // 2)
-                photo_bottom_y = self.paste_coords[1] + self.target_size[1] + 20
+                photo_bottom_y = self.paste_coords[1] + self.target_size[1] + 30
                 
                 text_width = get_text_width(font, name)
                 text_x = photo_center_x - (text_width // 2)
                 
                 # Ensure it doesn't go completely off-screen on the left even if max_width constraint failed
-                text_x = max(10, text_x)
+                text_x = max(20, text_x)
                 
                 draw.text((text_x, photo_bottom_y), name, font=font, fill="black")
                 
                 if institution:
-                    inst_font_size = font_size - 30
-                    if inst_font_size < 20: inst_font_size = 20
+                    inst_font_size = font_size - 20
+                    if inst_font_size < 24: inst_font_size = 24
                     inst_font = None
-                    while inst_font_size >= 10:
+                    while inst_font_size >= 20:
                         try:
                             inst_font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial.ttf", inst_font_size)
                         except:
@@ -90,11 +90,11 @@ class ImageProcessor:
                                 break
                         if get_text_width(inst_font, institution) <= max_text_width:
                             break
-                        inst_font_size -= 2
+                        inst_font_size -= 4
                         
                     inst_width = get_text_width(inst_font, institution)
                     inst_x = photo_center_x - (inst_width // 2)
-                    inst_x = max(10, inst_x)
+                    inst_x = max(20, inst_x)
                     draw.text((inst_x, photo_bottom_y + font_size + 10), institution, font=inst_font, fill="#555555")
             
             final_img.save(output_path, "PNG")
